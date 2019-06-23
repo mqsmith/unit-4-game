e799d6deec1b848818384e0c9595578a381d0aa9
Mike Smith <smith.mike@mac.com>
2019-06-23 10:04:52 -0400

    Generated random match number and the random value of each crystal. Also, added some formatting to the crystal pictures.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,35 @@
 
+
+$(document).ready(function () {
     // Selects a random number to be shown at the start of the game
     // Number should be should be between 19 - 120
-    //
-  
+    var random = Math.floor(Math.random() * 101 + 19);
+    console.log(random);
+
     // Appending random number to the randomNumber id in the html doc
-    //
-    
+    $("#randomNumber").text(random);
+
     // Setting up random numbers for each jewel
     // Random number has to be between 1 - 12
-    // 
-  
+    var num1 = Math.floor(Math.random() * 11 + 1)
+    var num2 = Math.floor(Math.random() * 11 + 1)
+    var num3 = Math.floor(Math.random() * 11 + 1)
+    var num4 = Math.floor(Math.random() * 11 + 1)
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+    console.log(num4);
+
     //  Decaring variables for tallies
-  
-  //resets the game
-  
-  //adds the wins to the userTotal
- 
-  //addes the losses to the userTotal
- 
-  //sets up click for jewels
- 
-            //sets win/lose conditions
-      
- 
+
+    //resets the game
+
+    //adds the wins to the userTotal
+
+    //addes the losses to the userTotal
+
+    //sets up click for jewels
+
+    //sets win/lose conditions
+
+});
