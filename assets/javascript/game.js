c4d1b5e232e5a273fa43f288526fac2c52053c14
Mike Smith <smith.mike@mac.com>
2019-06-23 11:07:53 -0400

    Put in click handlers, and called the functions that created earlier.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,14 +35,73 @@ $(document).ready(function() {
         num3 = Math.floor(Math.random() * 11 + 1);
         num4 = Math.floor(Math.random() * 11 + 1);
         userScore = 0;
+        $("#userScore").text("Your Score: " + userScore);
     }
 
     //adds the wins to the userTotal
-
+    function win (){
+        wins++;
+        alert("Winner, Winner!");
+        $("#wins").text("Wins: " + wins);
+        reset();
+    }
     //addes the losses to the userTotal
-
+    function lose (){
+        losses++;
+        alert("Whomp, Whomp!");
+        $("#losses").text("Losses: " + losses);
+        reset();
+    }
     //sets up click for jewels
+    $("#1").click(function(){
+        userScore = userScore + num1;
+    console.log("Your Score: " + userScore);
+    $("#userScore").text("Your Score: " + userScore); 
+          //sets win/lose conditions
+        if (userScore == random){
+          win();
+        }
+        else if ( userScore > random){
+          lose();
+        }   
+  })  
 
-    //sets win/lose conditions
+  $("#2").click(function(){
+    userScore = userScore + num2;
+console.log("Your Score: " + userScore);
+$("#userScore").text("Your Score: " + userScore); 
+      //sets win/lose conditions
+    if (userScore == random){
+      win();
+    }
+    else if ( userScore > random){
+      lose();
+    }   
+})  
+$("#3").click(function(){
+    userScore = userScore + num3;
+console.log("Your Score: " + userScore);
+$("#userScore").text("Your Score: " + userScore); 
+      //sets win/lose conditions
+    if (userScore == random){
+      win();
+    }
+    else if ( userScore > random){
+      lose();
+    }   
+})  
+$("#4").click(function(){
+    userScore = userScore + num4;
+console.log("Your Score: " + userScore);
+$("#userScore").text("Your Score: " + userScore); 
+      //sets win/lose conditions
+    if (userScore == random){
+      win();
+    }
+    else if ( userScore > random){
+      lose();
+    }   
+})  
+    
 
 });
